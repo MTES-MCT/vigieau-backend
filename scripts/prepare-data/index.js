41be13012bf4065fa86e5ab9fc398f636eb86cdd
Jérôme Desboeufs <jerome@livingdata.co>
2023-07-03 17:46:24 +0200

    Restrict processing to arrete with status Publié

--- scripts/prepare-data/index.js
+++ scripts/prepare-data/index.js
@@ -125,6 +125,7 @@ async function readArretes() {
       cheminFichierArreteCadre: row.chemin_fichier_arrete_cadre ? `${PROPLUVIA_DATA_URL}/pdf/${row.chemin_fichier_arrete_cadre}` : undefined
     }))
     .filter(arrete => arrete.dateDebutValidite <= today && arrete.dateFinValidite >= today)
+    .filter(arrete => arrete.statut_arrete === 'Publié')
     .groupBy('idArrete')
     .map(arreteZones => {
       const {idArrete, dateDebutValidite, dateFinValidite, cheminFichier, cheminFichierArreteCadre, statut} = arreteZones[0]
